fed3ba63660a40eee7a642a510058d4898c268bc
akyas <b00124354@gmail.com>
2026-04-28 00:24:21 +0700

    a

--- api/logout.php
+++ api/logout.php
@@ -5,7 +5,6 @@ if (session_status() === PHP_SESSION_NONE) session_start();
 session_unset();
 session_destroy();
 
-// Hapus Cookie Vercel
 setcookie('user_session', '', time() - 3600, "/");
 
 header("Location: /login");
